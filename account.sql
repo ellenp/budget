ebd9bd8a9e50f7141ec508336803998fed8a64d7
ellenp <pates.ellen@gmail.com>
2016-07-13 22:46:37 +0800

    Create account.sql

---
+++ account.sql
@@ -0,0 +1,15 @@
+CREATE TABLE account(
+  prefix_no int,
+  no int,
+  name varchar UNIQUE,
+  PRIMARY KEY (prefix_no, no)
+);
+
+CREATE TABLE account_transaction(
+  account_prefix_no int,
+  account_no int,
+  amount numeric(15, 2),
+  date_time timestamp,
+  FOREIGN KEY (account_prefix_no, account_no)
+    REFERENCES account (prefix_no, no)
+);
