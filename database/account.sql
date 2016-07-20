b45ac738f2b7b45f0664de10f8a33f762cde1221
ellenp <pates.ellen@gmail.com>
2016-07-20 11:29:01 +0800

    Update account.sql

--- database/account.sql
+++ database/account.sql
@@ -9,8 +9,45 @@ CREATE TABLE account_transaction(
   no int PRIMARY KEY,
   account_prefix_no int,
   account_no int,
-  amount numeric(15, 2),
+  credit_amount numeric(15, 2),
+  debit_amount numeric(15, 2),
   date_time timestamp,
   FOREIGN KEY (account_prefix_no, account_no)
     REFERENCES account (prefix_no, no)
 );
+
+CREATE FUNCTION create_account(
+  this_prefix_no int,
+  this_name varchar
+) RETURNS TABLE(
+  prefix_no int,
+  no int,
+  name varchar
+) AS $$
+  DECLARE
+  
+  BEGIN
+    
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION create_account_transaction(
+  this_no int,
+  this_account_prefix_no int,
+  this_account_no int,
+  this_credit_amount numeric(15, 2),
+  this_debit_amount numeric(15, 2)
+) RETURNS TABLE(
+  no int,
+  account_prefix_no int,
+  account_no int,
+  credit_amount numeric(15, 2),
+  debit_amount numeric(15, 2),
+  date_time timestamp
+) AS $$
+  DECLARE
+    this_date_time timestamp;
+  BEGIN
+    
+  END;
+$$ LANGUAGE plpgsql;
