7a42f5960f19ff50b2ba4b58ce84b090834e7c28
ellenp <pates.ellen@gmail.com>
2016-07-21 20:27:47 +0800

    Update account.sql

--- database/account.sql
+++ database/account.sql
@@ -1,46 +1,51 @@
 CREATE TABLE account(
-  prefix_no int,
-  no int,
+  prefix_id int,
+  id int,
   name varchar UNIQUE,
-  PRIMARY KEY (prefix_no, no)
+  PRIMARY KEY (prefix_id, id)
 );
 
 CREATE TABLE account_transaction(
-  no int PRIMARY KEY,
-  account_prefix_no int,
-  account_no int,
+  id int PRIMARY KEY,
+  account_prefix_id int,
+  account_id int,
   credit_amount numeric(15, 2),
   debit_amount numeric(15, 2),
   date_time timestamp,
-  FOREIGN KEY (account_prefix_no, account_no)
-    REFERENCES account (prefix_no, no)
+  FOREIGN KEY (account_prefix_id, account_id)
+    REFERENCES account (prefix_id, id)
 );
 
 CREATE FUNCTION create_account(
-  this_prefix_no int,
+  this_prefix_id int,
   this_name varchar
 ) RETURNS TABLE(
-  prefix_no int,
-  no int,
+  prefix_id int,
+  id int,
   name varchar
 ) AS $$
   DECLARE
   
   BEGIN
-    
+    INSERT INTO account(
+      prefix_id int,
+      id int,
+      name varchar UNIQUE,
+      PRIMARY KEY (prefix_id, id)
+    );
   END;
 $$ LANGUAGE plpgsql;
 
 CREATE FUNCTION create_account_transaction(
-  this_no int,
-  this_account_prefix_no int,
-  this_account_no int,
+  this_id int,
+  this_account_prefix_id int,
+  this_account_id int,
   this_credit_amount numeric(15, 2),
   this_debit_amount numeric(15, 2)
 ) RETURNS TABLE(
-  no int,
-  account_prefix_no int,
-  account_no int,
+  id int,
+  account_prefix_id int,
+  account_id int,
   credit_amount numeric(15, 2),
   debit_amount numeric(15, 2),
   date_time timestamp
@@ -53,4 +58,4 @@ CREATE FUNCTION create_account_transaction(
 $$ LANGUAGE plpgsql;
 
 
-INSERT INTO account (prefix_no, no, account) VALUES ()
+INSERT INTO account (prefix_id, id, account) VALUES ()
